5991c2c6edbd839821a48be1264ec1f5ba7c2413
Tristan Angeles <setswipe@gmail.com>
2021-09-21 16:06:16 -0400

    created and migrated band table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_183923) do
+ActiveRecord::Schema.define(version: 2021_09_21_194512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "year", null: false
+    t.integer "band_id", null: false
+    t.boolean "live", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_albums_on_title"
+  end
 
   create_table "bands", force: :cascade do |t|
     t.string "name", null: false
